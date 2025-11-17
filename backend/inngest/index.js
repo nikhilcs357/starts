7b9f0d0029acae522e6e9e18a58f7dd5a1f749c5
nikhilcs357 <nikhilcs044@gmail.com>
2025-11-17 20:38:26 +0530

    .

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -5,9 +5,11 @@ import sendEmail from "../config/nodeMailer.js";
 import Story from "../models/Story.js";
 import Message from "../models/Message.js";
 
+
+// Create a client to send and receive events
 export const inngest = new Inngest({ id: "sociofy-app" });
 
-// ====================== USER CREATION ========================
+// inngest function to save user data to a database
 const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
@@ -15,7 +17,8 @@ const syncUserCreation = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
     let username = email_addresses[0].email_address.split("@")[0];
 
-    const existingUser = await User.findOne({ username });
+    // Check availability of username
+    const existingUser = await User.findOne({ username }); 
 
     if (existingUser) {
       username = username + Math.floor(Math.random() * 10000);
@@ -29,11 +32,11 @@ const syncUserCreation = inngest.createFunction(
       username,
     };
 
-    await User.create(userData);
+    await User.create(userData); 
   }
 );
 
-// ====================== USER UPDATE ==========================
+// inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
@@ -42,34 +45,34 @@ const syncUserUpdation = inngest.createFunction(
 
     const updatedUserData = {
       email: email_addresses[0].email_address,
-      full_name: first_name + " " + last_name,
+      full_name: first_name + " " + last_name, 
       profile_picture: image_url,
     };
 
-    await User.findByIdAndUpdate(id, updatedUserData);
+    await User.findByIdAndUpdate(id, updatedUserData); 
   }
 );
 
-// ====================== USER DELETE ==========================
+// inngest function to delete user data to a database
 const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-from-clerk" },
-  { event: "clerk/user.deleted" },
+  { event: "clerk/user.deleted" }, 
   async ({ event }) => {
     const { id } = event.data;
 
-    await User.findByIdAndDelete(id);
+    await User.findByIdAndDelete(id); 
   }
 );
 
-// ================== CONNECTION REQUEST REMINDER ===================
+// inngest function to send remainder when a new connection request is added
 const sendNewConnectionRequestReminder = inngest.createFunction(
-  { id: "send-new-connection-request-reminder" },
-  { event: "app/connection-request" },
-  async ({ event, step }) => {
+  { id: "send-new-connection-request-reminder"},
+  { event: "app/connection-request"},
+  async({ event, step }) => {
     const { connectionId } = event.data;
-
-    await step.run("send-connection-request-mail", async () => {
-      const conn = await Connection.findById(connectionId).populate("from_user_id to_user_id");
+ 
+    await step.run('send-connection-request-mail', async () => {
+      const conn = await Connection.findById(connectionId).populate('from_user_id to_user_id'); 
 
       const subject = `New Connection Request`;
       const body = `
@@ -79,7 +82,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
          - @${conn.from_user_id.username}</p>
          <p>Click <a href="${process.env.FRONTEND_URL}/connections">here</a> to view it.</p>
          <p>Thanks.<br/>Sociofy - Stay Connected</p>
-      </div>`;
+      </div>`; 
 
       await sendEmail({
         to: conn.to_user_id.email,
@@ -91,8 +94,8 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
     const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
     await step.sleepUntil("wait-for-24-hours", in24Hours);
 
-    await step.run("send-connection-request-reminder", async () => {
-      const conn = await Connection.findById(connectionId).populate("from_user_id to_user_id");
+    await step.run('send-connection-request-reminder', async () => {
+      const conn = await Connection.findById(connectionId).populate('from_user_id to_user_id'); 
 
       if (conn.status === "accepted") {
         return { message: "Already accepted" };
@@ -106,7 +109,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
          - @${conn.from_user_id.username}</p>
          <p>Click <a href="${process.env.FRONTEND_URL}/connections">here</a> to respond.</p>
          <p>Thanks.<br/>Sociofy - Stay Connected</p>
-      </div>`;
+      </div>`; 
 
       await sendEmail({
         to: conn.to_user_id.email,
@@ -117,42 +120,40 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
       return { message: "Reminder sent." };
     });
   }
-);
+); 
 
-// ======================== DELETE STORY ============================
+// inngest function to delete story after 24 hours
 const deleteStory = inngest.createFunction(
-  { id: "story-delete" },
-  { event: "app/story.delete" },
-  async ({ event, step }) => {
+  {id: 'story-delete'},
+  {event: 'app/story.delete'},
+  async({event, step}) =>{
     const { storyId } = event.data;
-    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
-
-    await step.sleepUntil("wait-for-24-hours", in24Hours);
-
-    await step.run("delete-story", async () => {
-      await Story.findByIdAndDelete(storyId);
-      return { message: "story deleted." };
-    });
+    const in24Hours = new Date(Date.now()+ 24 * 60 * 60 * 1000)
+    await step.sleepUntil('wait-for-24-hours', in24Hours)
+    await step.run("delete-story", async()=> {
+      await Story.findByIdAndDelete(storyId)
+      return {message: "story deleted."}
+    })
   }
-);  
+)
 
-// =================== SEND UNSEEN MESSAGES ======================
 const SendNotificationOfUnseenMessages = inngest.createFunction(
-  { id: "send-unseen-messages-notification" },
-  { cron: "TZ=America/New_York 0 9 * * *" },
-  async ({ step }) => {
-    const messages = await Message.find({ seen: false }).populate("to_user_id");
-    const unseenCount = {};
+  {id: "send-unseen-messages-notification"},
+  {cron: "TZ=America/New_York 0 9 * * *" },
 
-    messages.map((message) => {
-      unseenCount[message.to_user_id._id] =
-        (unseenCount[message.to_user_id._id] || 0) + 1;
-    });
+  async ({step}) => {
+    const messages = await Message.find({seen: false}).populate('to_user_id');
+    const unseenCount = {}
 
-    for (const userId in unseenCount) {
+    messages.map(message=>{
+      unseenCount[message.to_user_id._id] = (unseenCount[message.to_user_id._id] || 0)
+      + 1;
+    })
+
+    for (const userId in unseenCount){
       const user = await User.findById(userId);
 
-      const subject = `💬 you have ${unseenCount[userId]} unseen messages`;
+      const subject = ` 💬 you have ${unseenCount[userId]} unseen messages`;
 
       const body = `
       <div style="font-family: Arial, sans-serif; padding: 20px;">
@@ -169,18 +170,18 @@ const SendNotificationOfUnseenMessages = inngest.createFunction(
         to: user.email,
         subject,
         body,
-      });
+      })
     }
-    return { message: "Notification sent." };
+    return {message: "Notification sent."}
   }
-);  
+)
 
-// ====================== EXPORT FUNCTIONS ======================
+//  Export all functions
 export const functions = [
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
   sendNewConnectionRequestReminder,
   deleteStory,
-  SendNotificationOfUnseenMessages,
-];
+  SendNotificationOfUnseenMessages
+]; 
