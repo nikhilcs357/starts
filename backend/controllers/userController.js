418237f316d0777a0f2a6deb8285e3ebd78b2ef4
nikhilcs357 <nikhilcs044@gmail.com>
2025-11-19 14:43:09 +0530

    .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -236,8 +236,8 @@ export const getUserConnections = async (req,res) => {
     const followers = user.followers
     const following = user.following
 
-    const pendingConnections = (await Connection.find({to_user_id: userId,
-      status: 'pending'}).populate('from_user_id')).map(connection => connection.from_user_id)
+    const pendingConnections = await Connection.find({to_user_id: userId,
+      status: 'pending'}).populate('from_user_id').map(connection => connection.from_user_id)
 
       res.json({success: true, connections, followers, following, pendingConnections})
     
